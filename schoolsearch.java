8e0a4a3d3caa807b32578ce4cc06cf7d4b1c9052
sbilich <sbilich@calpoly.edu>
2018-01-08 12:39:03 -0800

    basis for schoolsearch program

---
+++ schoolsearch.java
@@ -0,0 +1,19 @@
+/*
+* @author Sara Bilich and Makenna Johnstone
+* @date 1/8/18
+*/
+
+public static ArrayList<Student> students;
+
+public static void main(String args[]) {
+
+}
+
+/*
+* Parses in the students contained within FILENAME and
+* intializes |students|.
+*/
+private void parseStudents() {
+  students = new ArrayList<Student>();
+  
+}
