8aa4a1ecb94beaa1b0e56a5df19e6c184e20e041
Makenna Johnstone <makennajohnstone@Makennas-MacBook-Pro.local>
2018-01-12 10:16:12 -0800

    searchByLastName function working

--- schoolsearch.java
+++ schoolsearch.java
@@ -12,9 +12,12 @@ public class SchoolSearch {
   public static ArrayList<Student> students;
   public static final String FILENAME = "students.txt";
   public static final int STUDENT_SIZE = 8;
+  public static String [] userInput;
 
   public static void main(String args[]) {
     parseStudents();
+    promptSearchOptions();
+    initiateSearch();
   }
 
   /*
@@ -55,6 +58,7 @@ public class SchoolSearch {
         /* TODO should I check the strings for being not numbers? */
         Student student = new Student(last, first, grade, classroom, bus,
           gpa, tlast, tfirst);
+        students.add(student);
         //System.out.println(student);
       }
 
@@ -75,5 +79,59 @@ public class SchoolSearch {
       System.out.println("Scanning failed with exception " + e);
       System.exit(0);
     }
+  }
+
+  /*
+  * Gives the user a prompt to begin searching the students.
+  */
+  private static void promptSearchOptions() {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Choose to search from the following options:");
+    System.out.println("S[tudent]: <lastname> [B[us]]");
+    System.out.println("T[eacher]: <lastname>");
+    System.out.println("B[us]: <number>");
+    System.out.println("G[rade]: <number> [H[igh] | L[ow]]");
+    System.out.println("A[verage]: <number>");
+    System.out.println("I[nfo]");
+    System.out.println("Q[uit]");
+
+    //Splits the input string into an array
+    userInput = sc.nextLine().split(" ");
+  }
+
+  private static void initiateSearch() {
+    char firstLetter = userInput[0].charAt(0);
+
+    switch(firstLetter) {
+      case 'S':
+        if (userInput.length < 3) {
+          searchByLastName(userInput[1]);
+        }
+        else {
+          searchByLastNameFindBus(userInput[1]);
+        }
+    }
+  }
+  /*
+  * Given a student's last name, find the student's grade, classroom and teacher.
+  */
+  private static void searchByLastName(String lastName) {
+    ArrayList<Student> matchingStudents = new ArrayList<Student>();
+    for (Student stu: students) {
+      if (stu.getLastName().equals(lastName.toUpperCase())) {
+        matchingStudents.add(stu);
+      }
+    }
+
+    //print out student's grade, classroom and teacher
+    for (Student matchingStu: matchingStudents) {
+      System.out.println("Grade: " + matchingStu.getGrade());
+      System.out.println("Classroom: " + matchingStu.getClassroom());
+      System.out.println("Teacher: " + matchingStu.getTFirstName() + " " + matchingStu.getTLastName());
+    }
+  }
+  private static void searchByLastNameFindBus(String lastName) {
+
   }
 }
